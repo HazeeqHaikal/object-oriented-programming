02fd99017cb00ce2b3abb57b25911fd5f4bd587e
Hazeeq Haikal <43111765+HazeeqHaikal@users.noreply.github.com>
2022-08-07 12:44:49 +0800

    finished encapsilation

--- encapsulation/encapsulation.cpp
+++ encapsulation/encapsulation.cpp
@@ -2,7 +2,56 @@
 
 using namespace std;
 
+class Pekerja {
+   private:
+    string Name;
+    string Company;
+    int Age;
+
+   public:
+    string getCompany() {
+        return this->Company;
+    }
+    void setCompany(string company) {
+        this->Company = company;
+    }
+
+    int getAge() {
+        return this->Age;
+    }
+    void setAge(int age) {
+        if (age >= 18) {
+            this->Age = age;
+        }
+    }
+
+    string getName() {
+        return this->Name;
+    }
+    void setName(string name) {
+        this->Name = name;
+    }
+
+    void introduceYourself() {
+        cout << "Name: " << Name << endl;
+        cout << "Age: " << Age << endl;
+        cout << "Company: " << Company << endl;
+    };
+
+    Pekerja(string name, string company, int age) {
+        Name = name;
+        Company = company;
+        Age = age;
+    };
+};
+
 int main() {
-    
-    return 0;
+    Pekerja pekerja("Hazeeq", "Intel", 18);
+    pekerja.introduceYourself();
+
+    Pekerja pekerja2("Afrina", "TESL", 18);
+    pekerja2.introduceYourself();
+
+    pekerja.setAge(10);
+    cout << pekerja.getName() << " is " << pekerja.getAge() << " years old";
 }
